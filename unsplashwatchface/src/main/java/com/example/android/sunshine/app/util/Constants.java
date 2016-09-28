89b1281d07c2f2bd6fa6a38ae7f03277156200de
Adonis Arifi <adonisarifi@gmail.com>
2016-09-28 23:45:09 +0200

    style
    Comment line of constants...

--- unsplashwatchface/src/main/java/com/example/android/sunshine/app/util/Constants.java
+++ unsplashwatchface/src/main/java/com/example/android/sunshine/app/util/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
     public static final String FIREBASE_CLASS_BACKGROUND = "background";
 
 
-    //Weather
+    //Weather constants, used same on app
     public static final String WEATHER_PATH = "/weather";
     public static final String WEATHER_INFO_PATH = "/weather-info";
 
